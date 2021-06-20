eb87e5f916a14ddab159b3f4e587d81ceb8d3524
1822it1016 <55509487+1822it1016@users.noreply.github.com>
2021-06-20 19:01:29 +0530

    Add files via upload

---
+++ Day 9/1.js
@@ -0,0 +1,25 @@
+let total_tickets=25;
+let booked_tickets=0;
+let booked = document.getElementById("booked");
+let remaining = document.getElementById("total");
+remaining.innerHTML = total_tickets;
+booked.innerHTML = booked_tickets;
+
+
+const main = document.querySelector(".main");
+main.addEventListener("click",(e)=>{
+    var c = e.target.className;
+    if(c == "un"){
+        e.target.classList = "cl";
+        booked_tickets = +(booked_tickets) + 1;
+        total_tickets = +(total_tickets) - 1;
+
+    }if(c == "cl"){
+        e.target.classList = "un";
+        booked_tickets = +(booked_tickets) - 1;
+        total_tickets = +(total_tickets) + 1;
+
+    } 
+    remaining.innerHTML = total_tickets;
+    booked.innerHTML = booked_tickets;   
+});
