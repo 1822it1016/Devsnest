f6daee48ecd4065a11b6d070bac5ab6679ea8367
1822it1016 <55509487+1822it1016@users.noreply.github.com>
2021-08-11 17:34:15 +0530

    Add files via upload

---
+++ day_26/src/components/Output.js
@@ -0,0 +1,14 @@
+import {useSelector} from "react-redux";
+
+function Output() {
+  const username = useSelector(state => state.username.data)
+  const email = useSelector(state => state.email.data)
+  return (
+    <div className={"output"}>
+      Username: {username} <br/>
+      Email: {email}
+    </div>
+  );
+}
+
+export default Output;
