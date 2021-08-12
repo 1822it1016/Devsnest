60c7788b2e12ce137e688260cab5c94ce731240e
1822it1016 <55509487+1822it1016@users.noreply.github.com>
2021-08-12 11:08:17 +0530

    Add files via upload

---
+++ day_28/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Footer(props) {
+    return(
+        <footer role="contentinfo" className="footer">
+        </footer>
+    );
+}
+
+export default Footer;
